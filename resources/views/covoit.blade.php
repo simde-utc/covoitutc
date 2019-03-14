78b90ee28a74935a7ec1e0ef132c6c3570527b37
Youssef El Saadany <youssef.saadany@gmail.com>
2019-03-14 22:54:28 +0100

    Ajout des boutons 'Proposer/Chercher un trajet'

---
+++ resources/views/covoit.blade.php
@@ -0,0 +1,9 @@
+@extends('layout')
+
+@section('content')
+    <div class="content text-center">
+        <button type="button" class="btn btn-primary">Proposer un trajet</button>
+        <button type="button" class="btn btn-primary">Chercher un trajet</button>
+    </div>
+
+@endsection
