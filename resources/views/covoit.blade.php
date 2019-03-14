d7ae2c1df61bce476089e2b5e5970f8179b070f4
Youssef El Saadany <youssef.saadany@gmail.com>
2019-03-15 00:04:38 +0100

    Modif HTML

--- resources/views/covoit.blade.php
+++ resources/views/covoit.blade.php
@@ -4,9 +4,14 @@
     <div class="content text-center">
         <button id="proposerTrajet" type="button" class="btn btn-primary" onclick="proposerTrajet()">Proposer un trajet</button>
         <button id="chercherTrajet" type="button" class="btn btn-primary" onclick="chercherTrajet()">Chercher un trajet</button>
+        
+        <div id="proposerTrajetBlock" class="houdini">
+            <h2>Proposer un trajet</h2>
+        </div>
+        <div id="chercherTrajetBlock" class="houdini">
+            <h2>Chercher un trajet</h2>
+        </div>
     </div>
-    <div id="proposerTrajetBlock">
-        <p> bla bla bla </p>
-    </div>
+    
 
 @endsection
