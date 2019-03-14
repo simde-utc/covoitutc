65837389023d2f734976e3440c552dfc2b1c2974
Youssef El Saadany <youssef.saadany@gmail.com>
2019-03-15 00:03:48 +0100

    Ajout de deux fonctions JS pour faire apparaître les blocs

--- public/js/functions.js
+++ public/js/functions.js
@@ -1,4 +1,17 @@
 function proposerTrajet() {
     tmp = document.getElementById("proposerTrajetBlock");
-    tmp.style.display = "none";
+    tmp2 = document.getElementsByClassName("houdini");
+    for(var i=0 ; i<tmp2.length ; i++) {
+        tmp2[i].style.display = "none";
+    }
+    tmp.style.display = "block";
+}
+
+function chercherTrajet() {
+    tmp = document.getElementById("chercherTrajetBlock");
+    tmp2 = document.getElementsByClassName("houdini");
+    for(var i=0 ; i<tmp2.length ; i++) {
+        tmp2[i].style.display = "none";
+    }
+    tmp.style.display = "block";
 }
