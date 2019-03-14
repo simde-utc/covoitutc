175a6baa3fdc28fefcd504f77b737cd1fc81e838
Youssef El Saadany <youssef.saadany@gmail.com>
2019-03-14 23:54:29 +0100

    Faux texte pour test

--- resources/views/covoit.blade.php
+++ resources/views/covoit.blade.php
@@ -2,8 +2,11 @@
 
 @section('content')
     <div class="content text-center">
-        <button type="button" class="btn btn-primary">Proposer un trajet</button>
-        <button type="button" class="btn btn-primary">Chercher un trajet</button>
+        <button id="proposerTrajet" type="button" class="btn btn-primary" onclick="proposerTrajet()">Proposer un trajet</button>
+        <button id="chercherTrajet" type="button" class="btn btn-primary" onclick="chercherTrajet()">Chercher un trajet</button>
+    </div>
+    <div id="proposerTrajetBlock">
+        <p> bla bla bla </p>
     </div>
 
 @endsection
