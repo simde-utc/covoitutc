b9b91e9c5950e791614c5f83461a2eeb4f56cd52
Youssef El Saadany <youssef.saadany@gmail.com>
2019-03-15 11:28:30 +0100

    Fr vers En

--- public/js/functions.js
+++ public/js/functions.js
@@ -1,5 +1,5 @@
 function proposerTrajet() {
-    tmp = document.getElementById("proposerTrajetBlock");
+    tmp = document.getElementById("addRide");
     tmp2 = document.getElementsByClassName("houdini");
     for(var i=0 ; i<tmp2.length ; i++) {
         tmp2[i].style.display = "none";
@@ -8,7 +8,7 @@ function proposerTrajet() {
 }
 
 function chercherTrajet() {
-    tmp = document.getElementById("chercherTrajetBlock");
+    tmp = document.getElementById("searchRides");
     tmp2 = document.getElementsByClassName("houdini");
     for(var i=0 ; i<tmp2.length ; i++) {
         tmp2[i].style.display = "none";
