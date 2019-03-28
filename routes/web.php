67f19a155b9230fe86360333e4247b3e1d39517e
Youssef El Saadany <youssef.saadany@gmail.com>
2019-03-28 19:54:28 +0100

    Suppression du ProfileController et des routes vers ce controller

--- routes/web.php
+++ routes/web.php
@@ -21,9 +21,4 @@ Route::get('/covoit', function () {
 
 Route::post('/covoit', 'RideController@store');
 
-Route::get('/profile', 'ProfileController@index');
-Route::get('/profile/create', 'ProfileController@create');
-Route::post('/profile/create', 'ProfileController@store');
 
-Route::get('/profile/login', 'ProfileController@login');
-Route::post('/profile/verifyemail', 'ProfileController@login');
