41daac4f7d4a9e976a1b8554fbb305c1fc20ed44
Youssef El Saadany <youssef.saadany@gmail.com>
2019-06-14 14:24:47 +0200

    ajout d'une fonction pour récuperer les trajets exc en fonction du jour

--- app/Http/Controllers/RideController.php
+++ app/Http/Controllers/RideController.php
@@ -11,18 +11,16 @@ class RideController extends Controller
         // Faut gérer les profils /!\
         $tmp = request('recurrence');
         if($tmp == 'null') {
-            DB::insert("INSERT INTO exceptionnel(id, depart, arrivee, jour, heure, places, chauffeur) VALUES (:id, :destination, :departure, :day, :hour, :nb_seats, :driver)", [
-                'id' => 10,
+            DB::insert("INSERT INTO exceptionnel(depart, arrivee, jour, heure, places, chauffeur) VALUES (:departure, :destination, :daye, :hour, :nb_seats, :driver)", [
                 'departure' => request('departure'),
                 'destination' => request('destination'),
-                'day' => request('dateExc'),
+                'daye' => request('dateExc'),
                 'hour' => request('heureExc'),
                 'nb_seats' => request('nb_seats'),
                 'driver' => "lol"
             ]);
         } else {
-            DB::insert("INSERT INTO recurrents(id, depart, arrivee, places, heure, chauffeur, debut, type, jour, fin) VALUES(:id, :depart, :arrivee, :places, :heure, :chauffeur, :debut, :type, :jour, :fin)", [
-                'id' => 21,
+            DB::insert("INSERT INTO recurrents(depart, arrivee, places, heure, chauffeur, debut, type, jour, fin) VALUES(:depart, :arrivee, :places, :heure, :chauffeur, :debut, :type, :jour, :fin)", [
                 'depart' => request('departure'),
                 'arrivee' => request('destination'),
                 'places' => request('nb_seats'),
@@ -34,13 +32,34 @@ class RideController extends Controller
                 'fin' => request('dateFin')
             ]);
         }
-        $query = DB::table('recurrents')->get();
+        $query = DB::table('exceptionnel')->get();
         return $query;
     }
 
     public function showAllRides() {
-        $rides = DB::table('rides')->get();
+        //$rides = DB::table('exceptionnel')->get();
+        $rides = DB::select('SELECT * FROM Exceptionnel');
         return view('rides', [
+            'rides' => $rides
+        ]);
+    }
+
+    public function getExceptionnelByDate($jour) {
+        $rides = DB::select('SELECT * FROM Exceptionnel WHERE jour = :jour', [':jour' => $jour]);
+        
+        return $rides;
+    }
+
+    public function showRides() {
+        $depart = request('depart');
+        $arrivee = request('arrivee');
+
+        $rides = RideController::getExceptionnelByDate(request('date'));
+        return view('results', [
+            'depart' => $depart,
+            'arrivee' => $arrivee,
+            'date' => request('date'),
+            'heure' => request('heure'),
             'rides' => $rides
         ]);
     }
